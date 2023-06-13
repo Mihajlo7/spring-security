8c396753272b6d792461c613cedfe6ccedf11fd9
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-06-13 17:08:13 +0200

    Implementirana je metoda za generisanje novog tokena

--- src/main/java/com/pokusaj/security/config/JWTService.java
+++ src/main/java/com/pokusaj/security/config/JWTService.java
@@ -2,11 +2,16 @@ package com.pokusaj.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Key;
 import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class JWTService {
@@ -20,6 +25,18 @@ public class JWTService {
     public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
         final Claims claims=extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+    public String generateToken(Map<String,Object> extractClaims, UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
     }
     private Claims extractAllClaims(String token){
         return Jwts
