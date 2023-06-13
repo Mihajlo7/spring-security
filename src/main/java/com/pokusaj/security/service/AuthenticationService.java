97a197e937e850a19aa59104322da1ab10607f16
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-06-13 19:07:42 +0200

    Implementirana je metoda register

--- src/main/java/com/pokusaj/security/service/AuthenticationService.java
+++ src/main/java/com/pokusaj/security/service/AuthenticationService.java
@@ -1,16 +1,37 @@
 package com.pokusaj.security.service;
 
+import com.pokusaj.security.config.JWTService;
 import com.pokusaj.security.dto.AuthenticationRequest;
 import com.pokusaj.security.dto.AuthenticationResponse;
 import com.pokusaj.security.dto.RegisterRequest;
+import com.pokusaj.security.model.Role;
+import com.pokusaj.security.model.User;
+import com.pokusaj.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user= User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken=jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
