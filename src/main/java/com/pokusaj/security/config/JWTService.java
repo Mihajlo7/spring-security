955cfc27cbdeca1d0c453062835f205014433c1e
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-06-13 17:14:56 +0200

    Implementirana je metoda koja provera da li je isteklo trajanje tokena.

--- src/main/java/com/pokusaj/security/config/JWTService.java
+++ src/main/java/com/pokusaj/security/config/JWTService.java
@@ -39,6 +39,18 @@ public class JWTService {
         return generateToken(new HashMap<>(),userDetails);
     }
 
+    public boolean isTokenValid(String token,UserDetails userDetails){
+        final String username=extractUsername(token);
+        return(username.equals(userDetails.getUsername()) && !isTokenValid(token));
+    }
+    private boolean isTokenValid(String token){
+      return extractExpiration(token).before(new Date()) ;
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
+    }
+
     private Claims extractAllClaims(String token){
         return Jwts
                 .parserBuilder()
