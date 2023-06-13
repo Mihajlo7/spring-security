51d654fa7a670f0966ce8761c3367708bafd1dd7
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-06-13 17:22:35 +0200

    Implementirana klasa Filter

--- src/main/java/com/pokusaj/security/config/JWTAuthenticationFilter.java
+++ src/main/java/com/pokusaj/security/config/JWTAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +19,7 @@ import java.io.IOException;
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
     private final JWTService jwtService;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
@@ -31,5 +35,9 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
         }
         jwt=authHeader.substring(7);
         userEmail=jwtService.extractUsername(jwt);
+
+        if(userEmail!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+           // UserDetails userDetails=this.userDetailsService.loadByUsername(userEmail);
+        }
     }
 }
