b9a93247760e19e72b2fba343656c9ed3c0e8f9a
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-06-13 13:56:24 +0200

    Implementirana je klasa JWTService

--- src/main/java/com/pokusaj/security/config/JWTService.java
+++ src/main/java/com/pokusaj/security/config/JWTService.java
@@ -1,7 +1,31 @@
 package com.pokusaj.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Base64;
+
 public class JWTService {
+
+    private static final String SECRET_KEY="D77DA398955F942A4A6172BAD491E";
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
